f9d447d92448d5dc9c912264f26a3fe3c2db7314
AecioDev <espiranda.ms@gmail.com>
2024-11-26 21:30:32 -0400

    feat: Ajustado as Colunas da tabela de transações.

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -14,7 +14,7 @@ const TransactionsPage = async () => {
       {/* Titulo e Botão */}
       <div className="flex w-full justify-between items-center">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full">
+        <Button className="rounded-full font-bold">
           Adicionar Transação <ArrowDownUp />
         </Button>
       </div>
